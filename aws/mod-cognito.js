5913a833c6460104ccc01ce77abc39facc27989a
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-03-05 00:55:13 +0900

    Implement Cognito.SignIn

--- aws/mod-cognito.js
+++ aws/mod-cognito.js
@@ -1,10 +1,48 @@
+import { 
+    CognitoIdentityProviderClient, AuthFlowType,
+    InitiateAuthCommand
+} from "@aws-sdk/client-cognito-identity-provider";
+
 class ModAwsCognito {
 
-    async SignIn() {
+    async SignIn(request) {
 
         console.log("ModAwsCognito.SignIn");
+    
+        try {
+    
+            const client = new CognitoIdentityProviderClient({ region: request.RegionId });
+    
+            const command = new InitiateAuthCommand({
+                AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+                ClientId: request.ClientId,
+                AuthParameters: { USERNAME: request.UserId, PASSWORD: request.Password }
+            });
+    
+            console.log(`SignIn: UserId=${request.UserId}`);
+    
+            const response = await client.send(command);
+    
+            console.log(response);
+            console.log(response.AuthenticationResult.AccessToken);
+            console.log(response.AuthenticationResult.IdToken);
+            console.log(response.AuthenticationResult.RefreshToken);
+    
+            return {
+                Success: true,
+                IdToken: response.AuthenticationResult.IdToken,
+                AccessToken: response.AuthenticationResult.AccessToken,
+                RefreshToken: response.AuthenticationResult.RefreshToken
+            };
+        } catch (err) {
+            console.log("Error", err);
+            return {
+                Success: false,
+                ErrorMessage: err
+            };
+        }
 
-    };
+    }
 
 };
 
