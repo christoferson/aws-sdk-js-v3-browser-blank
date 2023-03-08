ef71b107f6111ab52349200c2d9c9afb42629789
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-03-08 23:41:33 +0900

    Add cognitoGetSecurityCredentials

--- main/app.js
+++ main/app.js
@@ -52,4 +52,64 @@ const cognitoSignIn = async () => {
 
 };
 
+
+const cognitoGetSecurityCredentials = async () => {
+
+    const viewResultElement = document.getElementById("get_security_credentials_result");
+
+    if (CognitoUserPoolCredentials.Authenticated === false) {
+        const msg = "CognitoGetSecurityCredentials: No Active Cognito Session. SignIn first.";
+        console.error(msg);
+        viewResultElement.innerHTML = `<p style='color:orange'>${msg}</p>`;
+        return;
+    }
+
+    if (SecurityCredentials.Authenticated === true) {
+        const msg = "CognitoGetSecurityCredentials: Credentials Already Active.";
+        console.error(msg);
+        viewResultElement.innerHTML = `<p style='color:orange'>${msg}</p>`;
+        return;
+    }
+
+    let SignInUserId = document.getElementById("sign_in_request_user_id").value;
+    SignInUserId = (SignInUserId)? SignInUserId : Settings.Cognito.UserId;
+
+    console.log(Settings.Cognito.UserPoolId);
+    console.log(CognitoUserPoolCredentials.Authenticated);
+    console.log(SecurityCredentials.Authenticated);
+
+    const modAwsCognito = new ModAwsCognito();
+    const response = await modAwsCognito.GetSecurityCredentials({
+        RegionId: Settings.Cognito.Region,
+        IdentityPoolId: Settings.Cognito.Identity.PoolId,
+        IdentityLoginProviderId: Settings.Cognito.Identity.LoginProviderId,
+        UserPoolIdToken: CognitoUserPoolCredentials.IdToken,
+        UserId: SignInUserId,
+        Password: Settings.Cognito.Password
+    });
+ 
+    console.log(response.Success);
+
+    if (response.Success) {
+        SecurityCredentials.signin(
+            response.AccessKeyId,
+            response.SecretKey,
+            response.SessionToken,
+            response.Expiration);
+        
+        viewResultElement.innerHTML = "Success";
+
+        document.getElementById("cognito_identity_pool_access_key_id").innerHTML = SecurityCredentials.AccessKeyId;
+        document.getElementById("cognito_identity_pool_secret_key").innerHTML = SecurityCredentials.SecretKey;
+        document.getElementById("cognito_identity_pool_session_token").innerHTML = SecurityCredentials.SessionToken;
+        document.getElementById("cognito_identity_pool_expiration").innerHTML = SecurityCredentials.Expiration;
+    } else {
+        viewResultElement.innerHTML = `<p style='color:red'>Error: ${response.ErrorMessage}</p>`
+
+        //document.getElementById("cognito_user_pool_authenticated").innerHTML = CognitoUserPoolCredentials.Authenticated;
+    }
+
+};
+
 window.cognitoSignIn = cognitoSignIn;
+window.cognitoGetSecurityCredentials = cognitoGetSecurityCredentials;
