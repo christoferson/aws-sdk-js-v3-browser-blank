5c38fddd9783b36c46f25e963ca47b2b29da72bf
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-03-10 20:03:55 +0900

    Add s3ListBucketObjects

--- main/app.js
+++ main/app.js
@@ -2,6 +2,8 @@ import { Settings } from "./settings.js";
 import { ModAws } from "../aws/mod.js";
 import { ModAwsCognito } from "../aws/mod-cognito.js";
 
+import { ModAwsS3 } from "../aws/mod-s3.js";
+
 const CognitoUserPoolCredentials = new ModAws.CognitoUserPoolCredentials();
 
 const SecurityCredentials = new ModAws.Credentials();
@@ -105,11 +107,66 @@ const cognitoGetSecurityCredentials = async () => {
         document.getElementById("cognito_identity_pool_expiration").innerHTML = SecurityCredentials.Expiration;
     } else {
         viewResultElement.innerHTML = `<p style='color:red'>Error: ${response.ErrorMessage}</p>`
+    }
 
-        //document.getElementById("cognito_user_pool_authenticated").innerHTML = CognitoUserPoolCredentials.Authenticated;
+};
+
+
+const s3ListBucketObjects = async () => {
+
+    const viewResultElement = document.getElementById("s3_list_bucket_objects_result");
+
+    if (CognitoUserPoolCredentials.Authenticated === false) {
+        const msg = "CognitoGetSecurityCredentials: No Active Cognito Session. SignIn first.";
+        console.error(msg);
+        viewResultElement.innerHTML = `<p style='color:orange'>${msg}</p>`;
+        return;
+    }
+
+    if (SecurityCredentials.Authenticated === false) {
+        const msg = "CognitoGetSecurityCredentials: No Active Security Credentials. Get Credentials first.";
+        console.error(msg);
+        viewResultElement.innerHTML = `<p style='color:orange'>${msg}</p>`;
+        return;
+    }
+
+    // let SignInUserId = document.getElementById("sign_in_request_user_id").value;
+    // SignInUserId = (SignInUserId)? SignInUserId : Settings.Cognito.UserId;
+
+    // console.log(Settings.Cognito.UserPoolId);
+    // console.log(CognitoUserPoolCredentials.Authenticated);
+    // console.log(SecurityCredentials.Authenticated);
+
+    const modAwsS3 = new ModAwsS3();
+    const response = await modAwsS3.ListBucketObjects({
+        AccessKeyId: SecurityCredentials.AccessKeyId,
+        SecretKey: SecurityCredentials.SecretKey,
+        SessionToken: SecurityCredentials.SessionToken,
+        BucketRegion: Settings.S3.Region,
+        BucketName: Settings.S3.BucketName
+    });
+
+    console.log(response.Success);
+
+    if (response.Success) {
+        
+        viewResultElement.innerHTML = "Success";
+
+        let s3ContentsFormatted = "";
+        response.BucketContents.forEach(element => {
+            s3ContentsFormatted += "  " + element.Key + " " + element.Size + "<br/>";
+        });
+
+        console.log(s3ContentsFormatted);
+
+        viewResultElement.innerHTML = `<p style='color:blue'>${s3ContentsFormatted}</p>`
+
+    } else {
+        viewResultElement.innerHTML = `<p style='color:red'>Error: ${response.ErrorMessage}</p>`
     }
 
 };
 
 window.cognitoSignIn = cognitoSignIn;
 window.cognitoGetSecurityCredentials = cognitoGetSecurityCredentials;
+window.s3ListBucketObjects = s3ListBucketObjects;
